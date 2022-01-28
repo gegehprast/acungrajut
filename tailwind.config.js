dee821d8e41fa78f786d3825a6d4626c1be6ffe1
gegehprast <gegeh.prast@gmail.com>
2022-01-28 16:38:32 +0700

    breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
         './components/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            
+        },
+
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '1366px': '1366px',
+            '2xl': '1536px',
+        }
     },
     plugins: [],
 }
