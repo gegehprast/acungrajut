5ac3a176c2e19ef6afa082d1812fa7254ba8454c
gegehprast <gegeh.prast@gmail.com>
2022-02-01 17:49:43 +0700

    stroke 3

--- components/icons/Search.tsx
+++ components/icons/Search.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Search = () => {
     return <svg xmlns="http://www.w3.org/2000/svg" className="w-full h-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
     </svg>
 }
 
