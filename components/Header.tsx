088e3738be606a8203d96f18f6af2725b93c8829
gegehprast <gegeh.prast@gmail.com>
2022-02-01 17:49:27 +0700

    header bg-secondary

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Banner from './Banner'
 
 const Header = () => {
-    return <header className="relative w-full h-screen overflow-hidden">
+    return <header className="relative w-full h-screen overflow-hidden bg-secondary">
         <Banner />
     </header>
 }
