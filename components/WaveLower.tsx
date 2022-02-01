69b7088eeaa4109c4dd5ace2d93717a7a39b3b0d
gegehprast <gegeh.prast@gmail.com>
2022-02-01 17:57:26 +0700

    waves

---
+++ components/WaveLower.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WaveLower = () => {
+    return <svg className="absolute bottom-0 right-0 w-full -z-10" viewBox="0 0 400 77" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M400 77L0 77V0C0 0 39.5 1 69 12.5C98.5 24 128.5 33 157 31C185.5 29 192.5 21 242 31C291.5 41 314 20.5 346 18C378 15.5 400 20.5 400 20.5L400 77Z" fill="#F06095"/>
+    </svg>
+
+
+}
+
+export default WaveLower
---
+++ components/WaveUpper.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const WaveUpper = () => {
+    return <svg className="absolute top-0 right-0 w-full -z-10" viewBox="0 0 400 77" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0 0H400V77C400 77 360.5 76 331 64.5C301.5 53 271.5 44 243 46C214.5 48 207.5 56 158 46C108.5 36 86 56.5 54 59C22 61.5 0 56.5 0 56.5L0 0Z" fill="#F06095"/>
+    </svg>
+
+}
+
+export default WaveUpper
