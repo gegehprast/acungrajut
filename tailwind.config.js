a4203701a9a908f76ffdf1cd74253e1c9c1ab190
gegehprast <gegeh.prast@gmail.com>
2022-02-01 17:49:17 +0700

    main drop shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
     theme: {
         extend: {
             dropShadow: {
+                'main-sm': '-1px 1px 1px rgba(240, 96, 149, 0.45)',
                 'main': '-1px 2px 2px rgba(240, 96, 149, 0.45)',
+                'main-md': '-1px 4px 3px rgba(240, 96, 149, 0.45)',
+                'main-lg': '-1px 10px 8px rgba(240, 96, 149, 0.45)',
+                'main-xl': '-1px 20px 13px rgba(240, 96, 149, 0.45)',
+                'main-2xl': '-1px 25px 25px rgba(240, 96, 149, 0.45)',
             },
 
             colors: {
