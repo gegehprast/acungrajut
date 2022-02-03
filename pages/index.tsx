f5a07fb67cc98f94dc22f8944eec72380baee52e
gegehprast <gegeh.prast@gmail.com>
2022-02-04 02:03:48 +0700

    products layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,79 +1,37 @@
 import Banner from '../components/Banner'
+import Item from '../components/Item'
 import WaveLower from '../components/WaveLower'
 import WaveUpper from '../components/WaveUpper'
 
+const items: Product[] = []
+
+for (let i = 0; i < 12; i++) {
+    const number = (i+1).toString().length > 1 ? i+1 : '0' + (i+1)
+
+    items.push({
+        name: `Lava Gray Hand Bag ${number} - Tas Jinjing`,
+        image: `/images/1200x1200/${number}.jpg`
+    })
+}
+
 export default function Home() {
     return (
-        <div className="relative w-full overflow-x-hidden">
-            <WaveUpper />
-            
-            <section className='w-full'>
-                <div className='font-nav font-semibold text-7xl w-full h-[100px] leading-none align-middle text-center text-white drop-shadow-main'>
-                    <span>Featured</span>
+        <div className="relative w-full">
+            <section className='relative w-full'>
+                <WaveUpper />
+
+                {/* section header */}
+                <div className='w-full text-5xl font-semibold leading-none text-center text-white md:p-5 font-nav md:text-7xl drop-shadow-main'>
+                    Featured
                 </div>
 
-                <div className='grid w-11/12 h-full grid-flow-cols lg:grid-flow-rows lg:grid-cols-3 gap-4 lg:gap-[60px] mx-auto text-white  lg:w-10/12'>
-                    <div>
-                        <div className='border-8 rounded-lg border-secondary aspect-square'>
-
-                        </div>
-                    </div>
-
-                    <div>
-                        <div className='border-8 rounded-lg border-secondary aspect-square'>
-
-                        </div>
-                    </div>
-
-                    <div>
-                        <div className='border-8 rounded-lg border-secondary aspect-square'>
-
-                        </div>
-                    </div>
-
-                    <div>
-                        <div className='border-8 rounded-lg border-secondary aspect-square'>
-
-                        </div>
-                    </div>
-
-                    <div>
-                        <div className='border-8 rounded-lg border-secondary aspect-square'>
-
-                        </div>
-                    </div>
-
-                    <div>
-                        <div className='border-8 rounded-lg border-secondary aspect-square'>
-
-                        </div>
-                    </div>
-
-                    <div>
-                        <div className='border-8 rounded-lg border-secondary aspect-square'>
-
-                        </div>
-                    </div>
-
-                    <div>
-                        <div className='border-8 rounded-lg border-secondary aspect-square'>
-
-                        </div>
-                    </div>
-
-                    <div>
-                        <div className='border-8 rounded-lg border-secondary aspect-square'>
-
-                        </div>
-                    </div>
+                {/* section body */}
+                <div className='grid w-11/12 h-full grid-flow-cols lg:grid-flow-rows md:grid-cols-3 gap-4 gap-y-10 md:gap-6 lg:gap-8 xl:gap-[60px] mx-auto text-white lg:w-10/12 pb-[48rem]'>
+                    {items.map((item, i) => <Item key={i} product={item} />)}
                 </div>
+
+                <WaveLower />
             </section>
-
-            <div className='mt-[48rem]'>
-
-            </div>
-
-            <WaveLower />
         </div>
     )
 }
